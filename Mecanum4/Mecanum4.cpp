b8e41704c5132232b575bafb8ff042474a41d04f
syhanjin <syhanjin666@gmail.com>
2026-01-31 18:09:27 +0800

    feat: add 4-wheel mecanum chassis support

---
+++ Mecanum4/Mecanum4.cpp
@@ -0,0 +1,207 @@
+/**
+ * @file    Mecanum4.cpp
+ * @author  syhanjin
+ * @date    2026-01-31
+ */
+#include "Mecanum4.hpp"
+
+namespace chassis
+{
+static constexpr size_t idx(Mecanum4::WheelType w)
+{
+    return static_cast<size_t>(w);
+}
+
+Mecanum4::Mecanum4(const Config& cfg) :
+    IChassis(cfg), type_(cfg.chassis_type), wheel_radius_(cfg.wheel_radius * 1e-3f)
+{
+    if (type_ == ChassisType::OType)
+        k_omega_ = cfg.wheel_distance_x * 1e-3f * 0.5f + cfg.wheel_distance_y * 1e-3f * 0.5f;
+    else if (type_ == ChassisType::XType)
+        k_omega_ = cfg.wheel_distance_x * 1e-3f * 0.5f - cfg.wheel_distance_y * 1e-3f * 0.5f;
+
+    wheel_[idx(WheelType::FrontRight)] = cfg.wheel_front_right;
+    wheel_[idx(WheelType::FrontLeft)]  = cfg.wheel_front_left;
+    wheel_[idx(WheelType::RearLeft)]   = cfg.wheel_rear_left;
+    wheel_[idx(WheelType::RearRight)]  = cfg.wheel_rear_right;
+}
+bool Mecanum4::enable()
+{
+    bool enabled = true;
+
+    for (const auto& w : wheel_)
+        enabled &= w->enable();
+
+    if (!enabled)
+    {
+        for (const auto& w : wheel_)
+            w->disable();
+    }
+    else
+    {
+        // 进行一次角度归零，用于计算正解
+        for (const auto& w : wheel_)
+            w->getMotor()->resetAngle();
+    }
+    enabled_ = enabled;
+    return enabled;
+}
+void Mecanum4::disable()
+{
+    for (const auto& w : wheel_)
+        w->disable();
+    enabled_ = false;
+}
+
+/**
+ * 设置底盘速度
+ */
+void Mecanum4::applyVelocity(const Velocity& velocity)
+{
+    const auto& [vx, vy, wz] = velocity;
+    if (type_ == ChassisType::OType)
+    {
+        /** Mecanum4 O 型运动学解算
+         * w_fr = (+ vx + vy + (w + h) * ω) / r
+         * w_fl = (+ vx - vy - (w + h) * ω) / r
+         * w_rl = (+ vx + vy - (w + h) * ω) / r
+         * w_rr = (+ vx - vy + (w + h) * ω) / r
+         */
+        wheel_[idx(WheelType::FrontRight)]->setRef(
+                RPS2RPM((vx + vy + k_omega_ * DEG2RAD(wz)) / wheel_radius_));
+        wheel_[idx(WheelType::FrontLeft)]->setRef(
+                RPS2RPM((vx - vy - k_omega_ * DEG2RAD(wz)) / wheel_radius_));
+        wheel_[idx(WheelType::RearLeft)]->setRef(
+                RPS2RPM((vx + vy - k_omega_ * DEG2RAD(wz)) / wheel_radius_));
+        wheel_[idx(WheelType::RearRight)]->setRef(
+                RPS2RPM((vx - vy + k_omega_ * DEG2RAD(wz)) / wheel_radius_));
+    }
+    else if (type_ == ChassisType::XType)
+    {
+        /** Mecanum4 X 型运动学解算
+         * w_fr = (+ vx - vy + (w - h) * ω) / r
+         * w_fl = (+ vx + vy - (w - h) * ω) / r
+         * w_rl = (+ vx - vy - (w - h) * ω) / r
+         * w_rr = (+ vx + vy + (w - h) * ω) / r
+         */
+        wheel_[idx(WheelType::FrontRight)]->setRef(
+                RPS2RPM((vx - vy + k_omega_ * DEG2RAD(wz)) / wheel_radius_));
+        wheel_[idx(WheelType::FrontLeft)]->setRef(
+                RPS2RPM((vx + vy - k_omega_ * DEG2RAD(wz)) / wheel_radius_));
+        wheel_[idx(WheelType::RearLeft)]->setRef(
+                RPS2RPM((vx - vy - k_omega_ * DEG2RAD(wz)) / wheel_radius_));
+        wheel_[idx(WheelType::RearRight)]->setRef(
+                RPS2RPM((vx + vy + k_omega_ * DEG2RAD(wz)) / wheel_radius_));
+    }
+}
+
+/**
+ * 底盘控制更新函数
+ *
+ * 本函数自动处理控制逻辑，并依序调用每个轮子的 PID 更新函数
+ *
+ * @note 推荐控制调用频率 1kHz，调用频率将会影响轮子的 PID 参数
+ */
+void Mecanum4::velocityControllerUpdate()
+{
+    if (!enabled())
+        return;
+    for (const auto& wheel : wheel_)
+        wheel->update();
+}
+
+float Mecanum4::forwardGetYaw()
+{
+    if (type_ == ChassisType::OType)
+        return wheel_radius_ / (4 * k_omega_) *
+               (wheel_[idx(WheelType::FrontRight)]->getMotor()->getAngle() -
+                wheel_[idx(WheelType::FrontLeft)]->getMotor()->getAngle() +
+                wheel_[idx(WheelType::RearRight)]->getMotor()->getAngle() -
+                wheel_[idx(WheelType::RearLeft)]->getMotor()->getAngle());
+
+    if (type_ == ChassisType::XType)
+        return wheel_radius_ / (4 * k_omega_) *
+               (wheel_[idx(WheelType::FrontRight)]->getMotor()->getAngle() -
+                wheel_[idx(WheelType::FrontLeft)]->getMotor()->getAngle() -
+                wheel_[idx(WheelType::RearRight)]->getMotor()->getAngle() +
+                wheel_[idx(WheelType::RearLeft)]->getMotor()->getAngle());
+
+    return 0.0f;
+}
+
+float Mecanum4::forwardGetX()
+{
+    return wheel_radius_ * 0.25f *
+           DEG2RAD(wheel_[idx(WheelType::FrontRight)]->getMotor()->getAngle() +
+                   wheel_[idx(WheelType::FrontLeft)]->getMotor()->getAngle() +
+                   wheel_[idx(WheelType::RearRight)]->getMotor()->getAngle() +
+                   wheel_[idx(WheelType::RearLeft)]->getMotor()->getAngle());
+}
+
+float Mecanum4::forwardGetY()
+{
+    if (type_ == ChassisType::OType)
+        return wheel_radius_ * 0.25f *
+               DEG2RAD(wheel_[idx(WheelType::FrontRight)]->getMotor()->getAngle() -
+                       wheel_[idx(WheelType::FrontLeft)]->getMotor()->getAngle() -
+                       wheel_[idx(WheelType::RearRight)]->getMotor()->getAngle() +
+                       wheel_[idx(WheelType::RearLeft)]->getMotor()->getAngle());
+
+    if (type_ == ChassisType::XType)
+        return wheel_radius_ / (4 * k_omega_) *
+               DEG2RAD(-wheel_[idx(WheelType::FrontRight)]->getMotor()->getAngle() +
+                       wheel_[idx(WheelType::FrontLeft)]->getMotor()->getAngle() +
+                       wheel_[idx(WheelType::RearRight)]->getMotor()->getAngle() -
+                       wheel_[idx(WheelType::RearLeft)]->getMotor()->getAngle());
+
+    return 0.0f;
+}
+
+float Mecanum4::forwardGetWz()
+{
+    if (type_ == ChassisType::OType)
+        return RPM2DPS(wheel_radius_ / (4 * k_omega_) *
+                       (wheel_[idx(WheelType::FrontRight)]->getMotor()->getVelocity() -
+                        wheel_[idx(WheelType::FrontLeft)]->getMotor()->getVelocity() +
+                        wheel_[idx(WheelType::RearRight)]->getMotor()->getVelocity() -
+                        wheel_[idx(WheelType::RearLeft)]->getMotor()->getVelocity()));
+
+    if (type_ == ChassisType::XType)
+        return RPM2DPS(wheel_radius_ / (4 * k_omega_) *
+                       (wheel_[idx(WheelType::FrontRight)]->getMotor()->getVelocity() -
+                        wheel_[idx(WheelType::FrontLeft)]->getMotor()->getVelocity() -
+                        wheel_[idx(WheelType::RearRight)]->getMotor()->getVelocity() +
+                        wheel_[idx(WheelType::RearLeft)]->getMotor()->getVelocity()));
+
+    return 0.0f;
+}
+
+float Mecanum4::forwardGetVx()
+{
+    return RPM2DPS(wheel_radius_ * 0.25f *
+                   DEG2RAD(wheel_[idx(WheelType::FrontRight)]->getMotor()->getVelocity() +
+                           wheel_[idx(WheelType::FrontLeft)]->getMotor()->getVelocity() +
+                           wheel_[idx(WheelType::RearRight)]->getMotor()->getVelocity() +
+                           wheel_[idx(WheelType::RearLeft)]->getMotor()->getVelocity()));
+}
+
+float Mecanum4::forwardGetVy()
+{
+    if (type_ == ChassisType::OType)
+        return RPM2DPS(wheel_radius_ * 0.25f *
+                       DEG2RAD(wheel_[idx(WheelType::FrontRight)]->getMotor()->getVelocity() -
+                               wheel_[idx(WheelType::FrontLeft)]->getMotor()->getVelocity() -
+                               wheel_[idx(WheelType::RearRight)]->getMotor()->getVelocity() +
+                               wheel_[idx(WheelType::RearLeft)]->getMotor()->getVelocity()));
+
+    if (type_ == ChassisType::XType)
+        return RPM2DPS(wheel_radius_ / (4 * k_omega_) *
+                       DEG2RAD(-wheel_[idx(WheelType::FrontRight)]->getMotor()->getVelocity() +
+                               wheel_[idx(WheelType::FrontLeft)]->getMotor()->getVelocity() +
+                               wheel_[idx(WheelType::RearRight)]->getMotor()->getVelocity() -
+                               wheel_[idx(WheelType::RearLeft)]->getMotor()->getVelocity()));
+
+    return 0.0f;
+}
+
+} // namespace chassis
---
+++ Mecanum4/Mecanum4.hpp
@@ -0,0 +1,84 @@
+/**
+ * @file    Mecanum4.hpp
+ * @author  syhanjin
+ * @date    2026-01-31
+ */
+#ifndef MECANUM4_HPP
+#define MECANUM4_HPP
+#include "IChassis.hpp"
+#include <cstddef>
+#include "motor_vel_controller.hpp"
+
+/**
+ * rad/s to round/min
+ * @param __RPS__ rad/s
+ */
+#define RPS2RPM(__RPS__) ((__RPS__) * 60.0f / (2 * 3.14159265358979323846f))
+
+#define DEG2RAD(__DEG__) ((__DEG__) * (float) 3.14159265358979323846f / 180.0f)
+
+#define RPM2DPS(__RPM__) ((__RPM__) / 60.0f * 360.0f)
+
+namespace chassis
+{
+
+class Mecanum4 final : public IChassis
+{
+public:
+    enum class WheelType : size_t
+    {
+        FrontRight = 0U, ///< 右前轮
+        FrontLeft,       ///< 左前轮
+        RearLeft,        ///< 左后轮
+        RearRight,       ///< 右后轮
+        Max
+    };
+    enum class ChassisType
+    {
+        XType = 0U, ///< X 型布局
+        OType,      ///< O 型布局
+    };
+
+    struct Config : IChassis::Config
+    {
+        float       wheel_radius;     ///< 轮子半径 (unit: mm)
+        float       wheel_distance_x; ///< 左右轮距 (unit: mm)
+        float       wheel_distance_y; ///< 前后轮距 (unit: mm)
+        ChassisType chassis_type;     ///< 底盘构型
+
+        controllers::MotorVelController* wheel_front_right; ///< 右前方
+        controllers::MotorVelController* wheel_front_left;  ///< 左前方
+        controllers::MotorVelController* wheel_rear_left;   ///< 左后方
+        controllers::MotorVelController* wheel_rear_right;  ///< 右后方
+    };
+
+    explicit Mecanum4(const Config& cfg);
+    bool enable() override;
+    void disable() override;
+    bool enabled() const override
+    {
+        return enabled_;
+    }
+
+protected:
+    void  applyVelocity(const Velocity& velocity) override;
+    void  velocityControllerUpdate() override;
+    float forwardGetX() override;
+    float forwardGetY() override;
+    float forwardGetYaw() override;
+    float forwardGetVx() override;
+    float forwardGetVy() override;
+    float forwardGetWz() override;
+
+private:
+    bool        enabled_{ false };
+    ChassisType type_;         ///< 底盘构型
+    float       wheel_radius_; ///< 轮子半径 (unit: m)
+    float       k_omega_;      ///< O 型：半宽 + 半高；X 型：半宽 - 半高 (unit: m)
+
+    controllers::MotorVelController* wheel_[static_cast<size_t>(WheelType::Max)];
+};
+
+} // namespace chassis
+
+#endif // MECANUM4_HPP
