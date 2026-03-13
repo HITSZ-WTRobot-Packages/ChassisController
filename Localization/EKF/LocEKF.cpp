0e599bb078c95a6bc3211b5582bc0e9e32ca03d7
syhanjin <syhanjin666@gmail.com>
2026-03-13 20:21:55 +0800

    feat(locEKF): 实现基于 deque 的时间重放更新

--- Localization/EKF/LocEKF.cpp
+++ Localization/EKF/LocEKF.cpp
@@ -1,5 +1,5 @@
 /**
- * @file    LocESKF.cpp
+ * @file    LocEKF.cpp
  * @author  syhanjin
  * @date    2026-03-11
  */
@@ -7,4 +7,147 @@
 
 namespace chassis_loc
 {
+LocEKF::PositionEKF::PositionEKF(const Config& cfg) :
+    EKF(cfg.x_init.vec(), cfg.covQ.mat(), cfg.noiseQ.mat()), R_gyro_(cfg.noiseR.gyro.mat()),
+    R_lidar_(cfg.noiseR.lidar.mat())
+{
+}
+
+auto LocEKF::PositionEKF::state() const
+{
+    return x;
+}
+
+auto LocEKF::PositionEKF::covariance() const
+{
+    return P;
+}
+
+void LocEKF::PositionEKF::reset(const VecS& x, const MatS& P)
+{
+    this->x = x;
+    this->P = P;
+}
+
+void LocEKF::PositionEKF::odomUpdate(const Velocity& vel, const float dt)
+{
+    auto next                = x;
+    const auto& [vx, vy, wz] = vel;
+
+    const auto yaw_w_rad = DEG2RAD(x[2] + x[3]);
+    const auto c = cosf(yaw_w_rad), s = sinf(yaw_w_rad);
+
+    next[0] += (vx * c - vy * s) * dt;
+    next[1] += (vx * s + vy * c) * dt;
+
+    MatS F  = MatS::identity();
+    F[0][2] = (-vx * s - vy * c) * dt;
+    F[0][3] = F[0][2];
+    F[1][2] = (vx * c - vy * s) * dt;
+    F[1][3] = F[1][2];
+
+    predict(next, F);
+}
+
+void LocEKF::PositionEKF::gyroUpdate(const float& yaw)
+{
+    // gyro 观测 yaw
+    using Vec1 = math::Vec<float, 1>;
+    constexpr math::Mat<float, 1, 4> H{ { { 0, 0, 1, 0 } } };
+    update(Vec1{ yaw - x[2] }, H, R_gyro_);
+}
+
+void LocEKF::PositionEKF::lidarUpdate(const Posture& pos)
+{
+    constexpr math::Mat<float, 3, 4> H{
+        {
+                { 1, 0, 0, 0 },
+                { 0, 1, 0, 0 },
+                { 0, 0, 1, 1 },
+        },
+    };
+    const math::Vec3f y{ pos.x - x[0], pos.y - x[1], pos.yaw - x[2] - x[3] };
+    update(y, H, R_lidar_);
+}
+
+void LocEKF::updateInput(const float yaw)
+{
+    const auto vel = chassis_->forwardGetVelocity();
+    // const auto yaw = gyro.getYaw();
+    // 将输入丢进缓冲区
+    input_buffer_.push_back({ HAL_GetTick(), vel, yaw });
+}
+
+void LocEKF::updateEKF()
+{
+    while (!input_buffer_.empty())
+    {
+        const auto& [ticks, vel, yaw] = input_buffer_[0];
+        input_buffer_.pop_front();
+        // 1. 更新预测
+        // TODO: HAL 库有没有什么办法告诉我 tick 更新频率的
+        pos_ekf_.odomUpdate(vel, dt());
+        // 2. 更新陀螺仪观测
+        pos_ekf_.gyroUpdate(yaw);
+        // 保存状态历史
+        state_buffer_.push_back({ .x     = pos_ekf_.state(),
+                                  .P     = pos_ekf_.covariance(),
+                                  .input = { .vel = vel, .yaw_gyro = yaw } });
+    }
+}
+
+void LocEKF::update(const float yaw)
+{
+    updateInput(yaw);
+    // 如果锁了，说明雷达更在更新，跳过本次更新，等雷达更新完调用更新 EKF
+    if (!lock_.load(std::memory_order_acquire))
+    {
+        updateEKF();
+        // 通过状态更新定位数据
+    }
+}
+
+void LocEKF::updateLidar(const Posture& pos, const uint32_t ticks)
+{
+    // 锁定更新状态, 此处假定 updateEKF 由中断调用，本身不会被打断
+    lock_.store(true, std::memory_order_acquire);
+
+    const uint32_t last_tick = state_buffer_.empty() ? 0 : state_buffer_.at(-1).input.ticks;
+
+    if (ticks >= last_tick)
+    {
+        // 直接更新
+        pos_ekf_.lidarUpdate(pos);
+        return;
+    }
+    const int state_tick = -static_cast<int>(
+            std::ceilf(static_cast<float>(last_tick - ticks) / static_cast<float>(dticks_)));
+    // 获取回溯状态
+    auto s = state_buffer_[state_tick];
+    // 回溯到之前的状态
+    pos_ekf_.reset(s.x, s.P);
+    // 更新雷达
+    pos_ekf_.lidarUpdate(pos);
+    s.x = pos_ekf_.state(), s.P = pos_ekf_.covariance();
+
+    for (auto& [x, P, input] : state_buffer_.range(state_tick + 1, 0))
+    {
+        // 重放并更新状态
+        const auto [_, vel, yaw] = input;
+        pos_ekf_.odomUpdate(vel, dt());
+        pos_ekf_.gyroUpdate(yaw);
+        x = pos_ekf_.state(), P = pos_ekf_.covariance();
+    }
+
+    lock_.store(false, std::memory_order_acquire);
+    // 手动更新堆积的输入
+    if (!input_buffer_.empty())
+        updateEKF();
+}
+
+LocEKF::LocEKF(const Config& cfg, sensors::gyro::HWT101CT& gyro, const uint32_t delta_ticks) :
+    gyro_(gyro), pos_ekf_(cfg), dticks_(delta_ticks)
+{
+}
+
 } // namespace chassis_loc
--- Localization/EKF/LocEKF.hpp
+++ Localization/EKF/LocEKF.hpp
@@ -7,11 +7,13 @@
  * 使用 fastlio2(10Hz) + encoder(1kHz) + HWT101CT(yaw, 1kHz) 融合定位
  */
 #pragma once
+#include "Deque.hpp"
 #include "HWT101CT.hpp"
 #include "ILoc.hpp"
 #include "Vec.hpp"
 #include "EKF.hpp"
 #include "Mat.hpp"
+#include <mutex>
 
 #ifndef DEG2RAD
 #    define DEG2RAD(__DEG__) ((__DEG__) * (float)3.14159265358979323846f / 180.0f)
@@ -32,9 +34,9 @@ namespace chassis_loc
 class LocEKF : public ILoc
 {
 public:
-    // sensors::gyro::HWT101CT& gyro;
+    sensors::gyro::HWT101CT& gyro_;
 
-    // TODO: 通过雷达反馈时间做回溯和重积分
+private:
     class PositionEKF : math::ekf::EKF<float, 4>
     {
         // state: [x, y, yaw, yaw_offset]
@@ -42,6 +44,8 @@ public:
         // yaw, yaw_offset: deg
         // wz: deg/s
     public:
+        using VecS = VecS;
+        using MatS = MatS;
         struct Config
         {
             // 初始值，初始清零offset
@@ -104,60 +108,61 @@ public:
             } noiseR;
         };
 
-        explicit PositionEKF(const Config& cfg) :
-            EKF(cfg.x_init.vec(), cfg.covQ.mat(), cfg.noiseQ.mat()), R_gyro_(cfg.noiseR.gyro.mat()),
-            R_lidar_(cfg.noiseR.lidar.mat())
-        {
-        }
+        explicit PositionEKF(const Config& cfg);
+        [[nodiscard]] auto state() const;
+        [[nodiscard]] auto covariance() const;
 
-        [[nodiscard]] auto state() const { return x; }
+        void reset(const VecS& x, const MatS& P);
 
-        void odomUpdate(const Velocity& vel, const float dt)
-        {
-            auto next                = x;
-            const auto& [vx, vy, wz] = vel;
+        void odomUpdate(const Velocity& vel, float dt);
 
-            const auto yaw_w_rad = DEG2RAD(x[2] + x[3]);
-            const auto c = cosf(yaw_w_rad), s = sinf(yaw_w_rad);
+        void gyroUpdate(const float& yaw);
 
-            next[0] += (vx * c - vy * s) * dt;
-            next[1] += (vx * s + vy * c) * dt;
-
-            MatS F  = MatS::identity();
-            F[0][2] = (-vx * s - vy * c) * dt;
-            F[0][3] = F[0][2];
-            F[1][2] = (vx * c - vy * s) * dt;
-            F[1][3] = F[1][2];
-
-            predict(next, F);
-        }
-
-        void gyroUpdate(const float& yaw, const float dt)
-        {
-            // gyro 观测 yaw
-            using Vec1 = math::Vec<float, 1>;
-            constexpr math::Mat<float, 1, 4> H{ { { 0, 0, 1, 0 } } };
-            update(Vec1{ yaw - x[2] }, H, R_gyro_);
-        }
-
-        void lidarUpdate(const Posture& pos, const float dt)
-        {
-            constexpr math::Mat<float, 3, 4> H{
-                {
-                        { 1, 0, 0, 0 },
-                        { 0, 1, 0, 0 },
-                        { 0, 0, 1, 1 },
-                },
-            };
-            const math::Vec3f y{ pos.x - x[0], pos.y - x[1], pos.yaw - x[2] - x[3] };
-            update(y, H, R_lidar_);
-        }
+        void lidarUpdate(const Posture& pos);
 
     private:
         math::Mat<float, 1, 1> R_gyro_;
         math::Mat<float, 3, 3> R_lidar_;
     };
     PositionEKF pos_ekf_;
+
+private:
+    struct Input
+    {
+        uint32_t ticks{ HAL_GetTick() };
+        Velocity vel{};      // 该状态时的预测输入
+        float    yaw_gyro{}; // 该状态时的陀螺仪观测输入
+    };
+
+    struct StatePoint
+    {
+        // 在输入完成之后的状态
+        PositionEKF::VecS x;
+        PositionEKF::MatS P;
+
+        Input input;
+    };
+
+    Deque<StatePoint, 1024> state_buffer_;
+    Deque<Input, 4>         input_buffer_;
+
+    uint32_t dticks_{ 1 };
+
+    std::atomic<bool>             lock_{ false };
+    [[nodiscard]] constexpr float dt() const { return static_cast<float>(dticks_) * 0.001f; }
+
+    void updateInput(float yaw);
+
+    void updateEKF();
+
+public:
+    using Config = PositionEKF::Config;
+
+    void update(float yaw);
+
+    void updateLidar(const Posture& pos, uint32_t ticks);
+
+    LocEKF(const Config& cfg, sensors::gyro::HWT101CT& gyro, uint32_t delta_ticks = 1);
 };
 
 } // namespace chassis_loc
